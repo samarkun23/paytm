5183c98fa16891a6c0435af64cf0d68dfcb441b7
samarkun <samarkun4@gmail.com>
2025-11-07 10:41:19 -0500

    add the min and max limit

--- backend/schema/userSchema.ts
+++ backend/schema/userSchema.ts
@@ -4,15 +4,19 @@ const userSchema = new mongoose.Schema({
     userName: {
         type: String,
         require: true,
+        min: 5,
+        max: 40
     },
     email: {
         type: String,
         require: true,
-        unique: true
+        unique: true,
+        min: 7
     },
     password: {
         type: String,
-        require: true
+        require: true,
+        min: 6
     }
 })
 
