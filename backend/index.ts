82bc1ad9b3487e0736d21f21ec56d211fbff45b0
samarkun <samarkun4@gmail.com>
2025-11-07 10:38:58 -0500

    server listen

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,19 @@
+import { mongoDB_Connect } from "./db.js";
+import express from 'express'
+import dotenv from 'dotenv'
 
+dotenv.config();
 
+const app = express();
+
+const PORT = process.env.PORT
+
+try {
+    mongoDB_Connect()
+    .then(() => app.listen(PORT, () => console.log(`Server is running on ${PORT}`))) 
+} catch (error) {
+    console.log(` Error while connecting with mongodb ${error}`)    
+}
 
 
 
