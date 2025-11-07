0a08363b4bea89a6ae23c951dc9dd7379e6707f3
samarkun <samarkun4@gmail.com>
2025-11-07 10:25:39 -0500

    userSchema

---
+++ backend/schema/userSchema.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    userName: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    }
+})
+
+export const User = mongoose.model("User", userSchema);
