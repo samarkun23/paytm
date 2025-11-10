3c499ce31184718eabb9aab17a545c98a4164ee4
samarkun <samarkun4@gmail.com>
2025-11-10 05:23:31 -0500

    userfilter route

---
+++ backend/router/v1/getUsersFilter.ts
@@ -0,0 +1,35 @@
+import { Router } from "express";
+import { User } from "../../schema/userSchema.js";
+
+const getUserFilters = Router();
+
+getUserFilters.get("/bulk", async function (req, res) {
+    const filter = req.query.filter || "";
+
+    try {
+        const filterUsers = await User.find({
+            $or: [
+                {
+                    userName: {
+                        "$regex": filter
+                    }
+                },
+                {
+                    email: {
+                        "$regex": filter
+                    }
+                }
+            ]
+        })
+
+        res.json({
+            user: filterUsers.map(user => ({
+                userName: user.userName,
+                email: user.email,
+                _id: user._id
+            }))
+        })
+    }catch{
+        res.status(404).json({message: "Filter user function not working"})
+    }
+})
