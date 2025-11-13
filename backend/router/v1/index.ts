c40161568cab4a0bfd8cf2f2b54b765c97b9ecb0
samarkun <samarkun4@gmail.com>
2025-11-13 13:46:09 -0500

    req goes to account router

--- backend/router/v1/index.ts
+++ backend/router/v1/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { userRouter } from "./userRouter.js";
+import { accountRouter } from "./accountRoutes.js";
 
 const mainRoute = Router();
 
 mainRoute.use("/user", userRouter);
+mainRoute.use("/account", accountRouter)
 
 export { mainRoute }
