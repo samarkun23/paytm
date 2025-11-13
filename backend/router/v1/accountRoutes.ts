fc5126da03b337e5221021e11e4554ec524e3c69
samarkun <samarkun4@gmail.com>
2025-11-13 13:48:43 -0500

    get balance route

--- backend/router/v1/accountRoutes.ts
+++ backend/router/v1/accountRoutes.ts
@@ -1,7 +1,18 @@
-import express from 'express'
+import express, { type Response } from 'express'
+import { Account } from '../../schema/accountSchema.js';
+import { authMiddeware } from '../../middleware/authMiddeware.js';
 
 const accountRouter = express.Router();
 
+//get there balance 
+accountRouter.get("/balance",authMiddeware, async function (req : any,res : Response) {
+    const account = await Account.findOne({
+        userId: req.userId
+    }) 
+    res.json({
+        balance: account?.balance
+    })
+})
 
 
 export {accountRouter}
